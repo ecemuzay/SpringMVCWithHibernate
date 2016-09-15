479306e77b468d49bd3dc1898b2d9939211a27c8
umutbuyukdurmus <umutugbaecem@gmail.com>
2016-09-15 15:36:18 +0300

    enabled security

--- src/main/java/com/spring/template/configuration/CustomUserDetailsService.java
+++ src/main/java/com/spring/template/configuration/CustomUserDetailsService.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service("customUserDetailsService")
+@Service("userDetailsService")
 public class CustomUserDetailsService implements UserDetailsService{
 
 	@Autowired
